b12520dab913ba2b71da1f7cb767ab253fd093e3
Siva Surya <siva.s@myn.global>
2022-08-13 12:37:39 +0530

    navbar done

--- src/App.js
+++ src/App.js
@@ -8,21 +8,22 @@ import {
   Header,
 } from "./containers";
 import { CTA, Brand, Navbar } from "./components";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
       <div className="gradient__bg">
-        <Navbar/>
-        <Header/>
+        <Navbar />
+        <Header />
       </div>
-      <Brand/>
-      <WhatGPT3/>
-      <Features/>
-      <Possibility/>
-      <CTA/>
-      <Blog/>
-      <Footer/>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 };
